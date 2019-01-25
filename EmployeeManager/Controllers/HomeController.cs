a8cb34267ef99cd9f8e035058be2b0b1684510d7
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-24 19:54:12 -0500

    Changed HomeController to log in employee

--- EmployeeManager/Controllers/HomeController.cs
+++ EmployeeManager/Controllers/HomeController.cs
@@ -5,15 +5,96 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using EmployeeManager.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Http;
 
 namespace EmployeeManager.Controllers
 {
     public class HomeController : Controller
     {
+        private EmployeeManagerContext _context;
+        public HomeController(EmployeeManagerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
         public IActionResult Index()
         {
             return View();
         }
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpGet("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost("register-employee")]
+        public IActionResult RegisterEmployee(Employee emp)
+        {
+            Employee CheckEmail = _context.Employees
+                .Where(e => e.Email == emp.Email)
+                .SingleOrDefault();
+            if(CheckEmail != null)
+            {
+                ViewBag.errors = "That email already exists";
+                return RedirectToAction("Register");
+            }
+            if(ModelState.IsValid)
+            {
+                PasswordHasher<Employee> Hasher = new PasswordHasher<Employee>();
+                Employee newEmp = new Employee
+                {
+                    Name = emp.Name,
+                    Email = emp.Email,
+                    Password = Hasher.HashPassword(emp, emp.Password),
+                    Active = true,
+                    Age = emp.Age,
+                    Title = emp.Title,
+                    Department = emp.Department,
+                    Salary = emp.Salary
+                };
+                _context.Add(newEmp);
+                _context.SaveChanges();
+                ViewBag.success = "Successfully registered";
+                return RedirectToAction("Login");
+            }
+            else
+            {
+                return View("Register");
+            }
+        }
+        [HttpPost("login-user")]
+        public IActionResult LoginEmployee(Employee emp)
+        {
+            Employee CheckEmail = _context.Employees
+                .SingleOrDefault(e => e.Email == emp.Email);
+            if (CheckEmail != null)
+            {
+                var Hasher = new PasswordHasher<Employee>();
+                if (0 != Hasher.VerifyHashedPassword(CheckEmail, CheckEmail.Password, emp.Password))
+                {
+                    HttpContext.Session.SetInt32("user_id", CheckEmail.Id);
+                    HttpContext.Session.SetString("first_name", CheckEmail.Name);
+                    return RedirectToAction("Dashboard");
+                }
+                else
+                {
+                    ViewBag.errors = "Incorrect Password";
+                    return View("Register");
+                }
+            }
+            else
+            {
+                ViewBag.errors = "Email not registered";
+                return View("Register");
+            }
+        }
+
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
@@ -23,3 +104,4 @@ namespace EmployeeManager.Controllers
         }
     }
 }
+
--- EmployeeManager/Startup.cs
+++ EmployeeManager/Startup.cs
@@ -20,25 +20,18 @@ namespace EmployeeManager
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
             services.AddDbContext<EmployeeManagerContext>(options => options.UseMySql(Configuration["DBInfo:ConnectionString"]));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSession();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
