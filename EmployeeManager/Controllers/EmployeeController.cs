597ce6a04e016e34179651356f88278d364b65d8
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-24 20:07:35 -0500

    Added some things to employee controller

--- EmployeeManager/Controllers/EmployeeController.cs
+++ EmployeeManager/Controllers/EmployeeController.cs
@@ -2,14 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmployeeManager.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeeManager.Controllers
 {
     public class EmployeeController : Controller
     {
-        public IActionResult Index()
+        private readonly EmployeeManagerContext _context;
+        public EmployeeController(EmployeeManagerContext context)
         {
+            _context = context;
+        }
+        private Employee ActiveEmployee
+        {
+            get
+            {
+                return _context.Employees.Where(e => e.Id == HttpContext.Session.GetInt32("Id")).FirstOrDefault(); 
+            }
+        }
+        public IActionResult Profile()
+        {
+            if(ActiveEmployee == null)
+            {
+                return RedirectToAction("Home", "Login");
+            }
+            ViewBag.emp = ActiveEmployee;
             return View();
         }
     }
