8cba534880f085e1757d81c95dce403718cce6b9
Kevin Stradtman <stradtkt22@gmail.com>
2019-01-25 14:33:34 -0500

    Added view model and login view

---
+++ EmployeeManager/Models/EmployeeViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeManager.Models
+{
+
+    public class RegisterUser
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [MinLength(3, ErrorMessage = "A minimum of 3 is allowed for name")]
+        [MaxLength(100, ErrorMessage = "A maximum of 100 is allowed for name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(4, ErrorMessage = "A minimum length of 4")]
+        [MaxLength(20, ErrorMessage = "A maximum length of 20")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("password")]
+        [Display(Name = "Confirm Password")]
+        public string Confirm { get; set; }
+    }
+
+    public class LoginUser
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "This is an email field")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(4, ErrorMessage = "A minimum length of 4")]
+        [MaxLength(20, ErrorMessage = "A maximum length of 20")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+
+        public string Password { get; set; }
+    }
+}
